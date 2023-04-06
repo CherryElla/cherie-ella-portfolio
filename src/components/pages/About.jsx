8654405136eedb7695f3b87df184e9e30d7b6652
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2023-04-06 13:19:03 +0100

    about section fix

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -12,14 +12,14 @@ export default function About() {
             <div className="flex flex-col px-20 justify-center sm:pl-32 text-gray-300 text-2xl mb-20 sm:text-left">
                 <p className="">
                     Hiya! I'm
-                    <span className="text-teal-200"> Cherie (Cherry) Ella</span>
+                    <span className="text-teal-200"> Cherie (Cherry) Ella</span>,
                     &nbsp;a Web Developer with endless excitement for technology
                     and all things futuristic. <br /> <br />
                     My lifelong fascination with computers, gaming, and internet
                     design has led me to journeying the realms of code and
                     solving ways to bring ideas to life. <br /> <br />
                     In 2023 I became a Full Stack Web Developer / Software
-                    Engineer, and now build websites and projects with
+                    Engineer and now build websites and projects with
                     JavaScript, Node.js, React and more. <br /> <br />
                     If I'm not breaking and building code, you can find me
                     immersed in physical art projects or in softwares like
